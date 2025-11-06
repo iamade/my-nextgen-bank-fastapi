af3479a393ae73e83585424bf04a8fa98c4e0e97
Adesegun Koiki <kamar3deen@gmail.com>
2025-11-05 18:18:40 -0700

    feat: logout

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from backend.app.api.routes import home
-from backend.app.api.routes.auth import register, activate, login, password_reset, refresh
+from backend.app.api.routes.auth import register, activate, login, password_reset, refresh, logout
 
 api_router = APIRouter()
 api_router.include_router(home.router)
@@ -9,3 +9,4 @@ api_router.include_router(activate.router)
 api_router.include_router(login.router)
 api_router.include_router(password_reset.router)
 api_router.include_router(refresh.router)
+api_router.include_router(logout.router)
